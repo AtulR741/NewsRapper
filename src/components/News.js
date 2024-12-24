8014d9fe6803bcfc519bd3b14d64c22af8c72368
AtulR741 <atul.raj300302@gmail.com>
2024-12-24 23:04:41 +0530

    inf scroll

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import NewsItem from "./NewsItem.js"
 import Spinner from "./Spinner.js";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     constructor(props) {
@@ -10,57 +11,48 @@ export default class News extends Component {
             page: 0,
             pageSize: 8,
             totalArticles: 0,
-            category: 'default',
-            loading: true
         };
     }
 
-    request = async (url) => {
-        this.setState({loading : true});
+    request = async () => {
+        this.setState({page: this.state.page + 1});
+        let url = `${this.props.state.finalUrl}&page=${this.state.page}&pageSize=${this.state.pageSize}`;
         let data = await fetch(url);
         let parseData = await data.json();
-        return parseData;
+        this.setState({articles: this.state.articles.concat(parseData.articles), totalArticles: parseData.totalResults});
     }
 
-    async componentDidMount() {
-        let parseData = await this.request(`${this.props.state.finalUrl}&page=1&pageSize=${this.state.pageSize}`);
-        this.setState({articles : parseData.articles, page : 1, totalArticles : parseData.totalResults, loading : false});
-    }
-
-    handleNext = async () => {
-        let parseData = await this.request(`${this.props.state.finalUrl}&page=${this.state.page + 1}&pageSize=${this.state.pageSize}`);
-        this.setState({articles : parseData.articles, page : this.state.page + 1, totalArticles : parseData.totalResults, loading : false});
-    }
-
-    handlePrev = async () => {
-        let parseData = await this.request(`${this.props.state.finalUrl}&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`);
-        this.setState({articles : parseData.articles, page : this.state.page - 1, totalArticles : parseData.totalResults, loading : false});
+    componentDidMount() {
+        this.request();
     }
 
     render() {
         return (
-            this.state.loading ? <Spinner /> :
-            this.state.totalArticles ?
-            <div className="container">
-                <h1 className="my-5" style={{ textAlign: 'center' }}>{this.props.state.header}</h1>
-                <div className="container my-3">
-                    <div className='row my-3'>
-                        {this.state.articles.map((element) => {
-                            return (
-                                <div key = {element.url} className="col">
-                                    <NewsItem title = {element.title} description = {element.description} imgUrl = {element.urlToImage} newsUrl = {element.url} author = {element.author} time = {element.publishedAt} source = {element.source.name} />
-                                </div>
-                            )
-                        })}
+            <InfiniteScroll
+                dataLength={this.state.articles.length}
+                next={this.request}
+                hasMore={this.state.articles.length < this.state.totalArticles}
+                loader={<Spinner />}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                    <b>No more results :P</b>
+                    </p>
+                }>
+                <div className="container">
+                    <h1 className="my-5" style={{ textAlign: 'center' }}>{this.props.state.header}</h1>
+                    <div className="container my-3">
+                        <div className='row my-3'>
+                            {this.state.articles.map((element, index) => {
+                                return (
+                                    <div key = {index} className="col">
+                                        <NewsItem title = {element.title} description = {element.description} imgUrl = {element.urlToImage} newsUrl = {element.url} author = {element.author} time = {element.publishedAt} source = {element.source.name} />
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
-                <div className="d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark mx-3 my-3" onClick={this.handlePrev}>&larr; Prev</button>
-                    <button disabled={this.state.page >= Math.ceil(this.state.totalArticles/this.state.pageSize)} type="button" className="btn btn-dark mx-3 my-3" onClick={this.handleNext}>Next &rarr;</button>
-                </div>
-            </div>
-            :
-            <h1 className="my-5" style={{ textAlign: 'center' }}>Nothing to show</h1>
+            </InfiniteScroll>
         );
     }
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Spinner extends Component {
     render() {
         return (
-            <div className='text-center loading'>
+            <div className='text-center'>
                 <div className="spinner-grow text-dark" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>
